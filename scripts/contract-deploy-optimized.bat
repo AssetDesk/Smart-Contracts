87e287d98e18e54e133b92d58400ccb805f1b0a3
Dmytro Biriukov <dimbirua@gmail.com>
2023-10-25 14:21:21 +0300

    cli scripts for new contract

---
+++ scripts/contract-deploy-optimized.bat
@@ -0,0 +1,5 @@
+soroban contract deploy^
+    --wasm target\wasm32-unknown-unknown\release\soroban_lending.optimized.wasm^
+    --source SDQFFLWT4HGC36UO3U3H7GHRFTEVKWAOWZNDJ2C4NRHH7TY2PN4QLNSN^
+    --rpc-url https://rpc-futurenet.stellar.org:443^
+    --network-passphrase "Test SDF Future Network ; October 2022"
--- scripts/contract-optimize.bat
+++ scripts/contract-optimize.bat
@@ -1,1 +1,2 @@
+cargo build --target wasm32-unknown-unknown --release --no-default-features
 soroban contract optimize --wasm target\wasm32-unknown-unknown\release\soroban_lending.wasm
