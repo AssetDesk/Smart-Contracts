0b7542c96f4d21d13bc41467a97e0c71a958d6a4
Dmytro Biriukov <dimbirua@gmail.com>
2023-10-25 08:40:38 +0300

    scripts: init and price

---
+++ scripts/contract-get-price.bat
@@ -0,0 +1,7 @@
+soroban contract invoke^
+  --id CCJFMEIHPQDCBO3B77NSCFEE32CXBEA6ZWPTTOCQQ6ONHQDFHSSANQ2U^
+  --source SDNTGAOPMR2VKBJUKYHWJOCCNBJA4EAXG5FTGFGHZPGPWLZM7TF63O5J^
+  --rpc-url https://rpc-futurenet.stellar.org:443/^
+  --network-passphrase "Test SDF Future Network ; October 2022"^
+  -- GetPrice^
+  --denom xlm
---
+++ scripts/contract-update-price.bat
@@ -0,0 +1,8 @@
+soroban contract invoke^
+  --id CCJFMEIHPQDCBO3B77NSCFEE32CXBEA6ZWPTTOCQQ6ONHQDFHSSANQ2U^
+  --source SDNTGAOPMR2VKBJUKYHWJOCCNBJA4EAXG5FTGFGHZPGPWLZM7TF63O5J^
+  --rpc-url https://rpc-futurenet.stellar.org:443/^
+  --network-passphrase "Test SDF Future Network ; October 2022"^
+  -- UpdatePrice^
+  --denom xlm^
+  --price 11360000
